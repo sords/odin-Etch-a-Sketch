23fa094c16aa60cc8049894c1779cc06cb57c45a
sampath <sampathsampath0852@gmail.com>
2022-08-19 19:48:56 +0530

    added ui and features

--- script.js
+++ script.js
@@ -1,69 +1,65 @@
 let grid = document.querySelector('#grid');
 let number = document.getElementById('number');
-let boxes = document.querySelectorAll('#pixel');
-
-
+let pixels = document.querySelectorAll('#pixel');
+let pixelscount = 0;
+let previous;
+let color = document.getElementById('favcolor')
 
 function box(num) {
-    console.log(num)
-    console.log(number.value)
-     
-     let box = boxes.length;
-     console.log(boxes.length,1,box)
-
-
-    if (box > 0) {
-        for (let j = 0; j < num * num; j++) {
-            let child = document.getElementById("pixel");
-            grid.removeChild(child)
-            box--;
-            console.log(box,99)
+    if (num > 100 || num <= 0) {
+        alert('please enter between 0 and 100')
+    } else {
+        if (pixelscount >= 1) {
+            for (let j = 0; j < previous; j++) {
+                let child = document.getElementById("pixel");
+                grid.removeChild(child)
+                pixelscount -= 1;
+            }
         }
-    } //else {
         for (let j = 0; j < num * num; j++) {
             let div = document.createElement('div')
-            div.style.height = 500 / num + 'px'
+            div.style.height = (500 / num) + 'px'
             div.style.width = 500 / num + 'px'
             grid.appendChild(div)
             div.setAttribute('id', 'pixel');
-            box++;
-            
-       // }
-
+            pixelscount += 1;
+        }
+        previous = pixelscount;
+        addcolor()
     }
-    console.log(boxes.length,2,box)
 }
-
-    box(number.value)
-
-
-
-
-
-/*function changethegrid(num) {
-    for (let j = 0; j < num*num; j++) {
-        let child = document.getElementById("pixel");
-        grid.removeChild(child)
-    }
-    let numbe = document.getElementById('number');
-    box(numbe.value);
-}
-
 function addcolor() {
-    boxes.forEach(pixel => {
+    let pixels = document.querySelectorAll('#pixel')
+    pixels.forEach(pixel => {
         pixel.addEventListener('mouseover', function () {
-            pixel.style.background = 'black'
+            pixel.style.background = `${color.value}`;
         })
     })
 }
-
-function cleargrid() {
-    boxes.forEach(pixel => {
+function Earasegrid() {
+    let pixels = document.querySelectorAll('#pixel')
+    pixels.forEach(pixel => {
         pixel.addEventListener('mouseover', function () {
             pixel.style.background = 'white'
         })
     })
 }
+function cleargrid() {
+    let pixels = document.querySelectorAll('#pixel')
+    pixels.forEach(pixel => {
+        pixel.style.background = 'white'
 
-addcolor()
-*///
+    })
+    addcolor()
+}
+
+function random() {
+    let pixels = document.querySelectorAll('#pixel')
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseover', function () {
+            pixel.style.background = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`
+        })
+    })
+}
+
+box(number.value);
